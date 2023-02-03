9492ad168e1b5d3bc334f0b7c4c4c71bba9846bc
Abhay Kapatia <abhay2001kapatia@gmail.com>
2023-02-04 01:42:34 +0530

    day1

--- noOfNotes.cpp
+++ noOfNotes.cpp
@@ -2,8 +2,35 @@
 using namespace std;
 int main()
 {
-    int n;
+    int n,c=0;
     cin >> n;
-    
+    if (n>=100){
+        c=n/100;
+        cout<<"100 : "<<c<<endl;
+        n=n-c*100;
+    }
+    if (n>=50){
+        c=n/50;
+        cout<<"50 : "<<c<<endl;
+        n=n-c*50;
+    }
+    if (n>=20){
+        c=n/20;
+        cout<<"20 : "<<c<<endl;
+        n=n-c*20;
+    }
+    if (n>=10){
+        c=n/10;
+        cout<<"10 : "<<c<<endl;
+        n=n-c*10;
+    }
+    if (n<10){
+        int c =0;
+        while(n){
+            c++;
+            n--;
+        }
+        cout<<"remain : "<<c<<endl;
+    }
     return 0;
 }
