696d50244387ffa4863f6ebde0908e202a3fabb9
Abhay Kapatia <abhay2001kapatia@gmail.com>
2023-04-04 13:40:18 +0530

    array day 2

--- 2-Array/arrayUnique.cpp
+++ 2-Array/arrayUnique.cpp
@@ -1,13 +1,28 @@
 #include<iostream>
+#include <unordered_map>
 using namespace std;
+//find unique elements in an array
 int main()
 {
     int a[10]={};
+    int c[10]={};
+
     for(int i=0; i<10 ; i++){
         cin >>a[i];
     }
+    unordered_map<int, int> umap;
     for(int i=0;i<10;i++){
-        
+        umap[a[i]]=0;
     }
+    for(int i=0;i<10;i++){
+        umap[a[i]]+=1;
+    }
+    cout<<endl<<endl;
+    for(int i=0;i<10;i++){
+        if(umap[a[i]]==1){
+            cout<<a[i]<<" ";
+        }        
+    }
+    
     return 0;
 }
